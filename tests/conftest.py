9ec5b4a25df4f096eb27f134131b0a58d079e0af
Marcelo Pontes <infopontes@gmail.com>
2023-09-15 23:44:25 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,50 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import sessionmaker
 
 from sandfly.app import app
-from sandfly.models import Base
+from sandfly.database import get_session
+from sandfly.models import Base, User
+
+# @pytest.fixture
+# def client():
+#     return TestClient(app)
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
-    Session = sessionmaker(bind=engine)
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
     yield Session()
+
     Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
