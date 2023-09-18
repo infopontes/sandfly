db778534d8be69a34d05b6831df6b01382eaf458
Marcelo Pontes <infopontes@gmail.com>
2023-09-17 21:38:50 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

---
+++ sandfly/routes/auth.py
@@ -0,0 +1,35 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from sandfly.database import get_session
+from sandfly.models import User
+from sandfly.schemas import Token
+from sandfly.security import create_access_token, verify_password
+
+OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+Session = Annotated[Session, Depends(get_session)]
+
+router = APIRouter(tags=['token'])
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(form_data: OAuth2Form, session: Session):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,13 +1,16 @@
 from jose import jwt
 
-from sandfly.security import SECRET_KEY, create_access_token
+from sandfly.security import create_access_token
+from sandfly.settings import Settings
+
+settings = Settings()
 
 
 def test_jwt():
     data = {'test': 'test'}
     token = create_access_token(data)
 
-    decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    decoded = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
 
     assert decoded['test'] == data['test']
     assert decoded['exp']  # Testa se o valor de exp foi adicionado ao token
